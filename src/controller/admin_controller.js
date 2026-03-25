13d24cd7cfa5cf7c639d0fcb5b4975c700acd63f
aj-codess <jagyei339@gmail.com>
2026-03-25 03:54:57 -0500

    progress

---
+++ src/controller/admin_controller.js
@@ -0,0 +1,35 @@
+
+let getAllUsers = (req,res) => {
+    try{
+
+    } catch(error){
+
+    };
+};
+
+
+
+let activate = () => {
+    try{
+
+    } catch(error){
+
+    };
+};
+
+
+
+let deactivate = () => {
+    try{
+
+    } catch(error){
+
+    };
+};
+
+
+export default {
+    getAllUsers,
+    activate,
+    deactivate,
+}
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,4 +1,6 @@
 import express from "express";
+import admin_controller from "./../controller/admin_controller.js";
+import auth_mid from "./../middleware/authenticator.js";
 
 
 const admin = express.Router();
@@ -8,18 +10,19 @@ const admin = express.Router();
 
 // get all users
 admin.get("/users/:offset/:limit",(req,res)=>{
-
+    // manually use middleware here
+    admin_controller.getAllUsers(req,res);
 });
 
 
 // activate a users account
 admin.patch("/users/:id/activate",(req,res)=>{
-
+    admin_controller.activate(req,res);
 });
 
 // deactivate
 admin.patch("/users/:id/dactivate",(req,res)=>{
-
+    admin_controller.deactivate(req,res);---
 });
 
 
