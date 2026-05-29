2c975c5ab9a30b36c6ad1dc1343891a5cffc636b
aj-codess <jagyei339@gmail.com>
2026-05-29 06:17:10 -0500

    progress

--- src/controller/organizations_controller.js
+++ src/controller/organizations_controller.js
@@ -93,9 +93,29 @@ let create_org = async (req,res) => {
             creator_id : req.user
         };
 
-        
+        const created_payload = await org_model.create_org(obj);
+
+        if(created_payload.status==false && created_payload.conflict==true){
+            return res.status(409).json(created_payload);
+        } else if(created_payload.status==true){
+            return res.status(200).json(created_payload);
+        } else{
+            return res.status(500).json(created_payload);
+        };
 
     } catch(error){
+
+        console.error({
+            system:"Internal Server Error At Creating Organization Model",
+            stack:error.stack,
+            name:error.name,
+            message:error.message
+        });
+
+        return res.status(500).json({
+            status:false,
+            message:"Internal Server Error Creating Organization"
+        });
 
     };
 };
@@ -105,7 +125,33 @@ let create_org = async (req,res) => {
 let get_org_via_slug = async (req,res) => {
     try{
 
+        const obj = {
+            slug:req.params.slug
+        };
+
+        const payload = await org_model.get_org_via_slug(obj);
+
+        if(payload.status==false && payload.not_found==true){
+            return res.status(404).json(payload);
+        } else if(payload.status==true){
+            return res.status(200).json(payload);
+        } else{
+            return res.status(500).json(payload);
+        };
+
     } catch(error){
+
+        console.error({
+            system:"Internal Server Error At Get Organization Via Slug Model",
+            stack:error.stack,
+            name:error.name,
+            message:error.message
+        });
+
+        return res.status(500).json({
+            status:false,
+            message:"Internal Server Error Getting Organization Via Slug"
+        });
 
     };
 };
@@ -114,6 +160,21 @@ let get_org_via_slug = async (req,res) => {
 
 let update = async (req,res) => {
     try{
+
+        const {name,slug,description,website_url,contact_email,access_mode,org_id} = req.body;
+
+        const obj = {
+            name:name?.trim(),
+            slug:slug?.trim(),
+            description:description?.trim().toLowerCase(),
+            website_url:website_url?.trim(),
+            contact_email:contact_email?.trim(),
+            access_mode:access_mode?.trim(),
+            org_id,
+            caller_id:req.user
+        };
+
+        const payload = await org_model.update(obj);
 
     } catch(error){
 
